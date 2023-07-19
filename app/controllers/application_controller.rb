1b075621ae7d9a895f4a620b47c4b32715d22967
EC2 Default User <ec2-user@ip-172-31-43-62.ap-northeast-1.compute.internal>
2023-07-19 11:47:37 +0000

    0719

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
@@ -13,6 +14,12 @@ class ApplicationController < ActionController::Base
     flash[:notice] = "Signed out successfully."
     user_session_path
   end
+  
+  def after_sign_up_path_for(resource)
+    flash[:notice] = "Signed up successfully."
+    books_path
+  end
+  
 
   protected
 
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,20 +21,29 @@ class BooksController < ApplicationController
   end
 
   def create
+
+    @new_book = Book.new
+    @books = Book.all
+    @user = current_user
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-    redirect_to books_path
+    flash[:notice] = "You have crated book successfully."
+    redirect_to book_path(@book.id)
     else
     render :index
     end
 
   end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
     redirect_to book_path(book.id)
+    else
+    render :edit
+    end
   end
 
   def destroy
