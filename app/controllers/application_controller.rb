26b0ab2adf39959e0c557ac3baa4efd27d282a9d
EC2 Default User <ec2-user@ip-172-31-43-62.ap-northeast-1.compute.internal>
2023-07-22 11:04:31 +0000

    finish

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     flash[:notice] = "Signed in successfully."
-    user_path(@user.id)
+    user_path(current_user)
   end
 
 
