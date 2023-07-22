f296436b0052ae968392a55a21f945a973b9812a
EC2 Default User <ec2-user@ip-172-31-43-62.ap-northeast-1.compute.internal>
2023-07-22 06:20:05 +0000

    error50

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
 
          has_one_attached :profile_image
 
-         validates :name, {presence: true, uniqueness: true, length:{minimum: 2, maximum: 20 }}
-         validates :introduction, {presence: true, length:{maximum: 50 }}
+         validates :name, {uniqueness: true, length:{minimum: 2, maximum: 20 }}
+         validates :introduction, {length:{maximum: 50 }}
 
 def get_profile_image(width, height)
   unless profile_image.attached?
